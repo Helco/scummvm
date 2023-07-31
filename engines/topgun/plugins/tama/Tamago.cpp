419ee1beb2bd72f7a04d0ea384b769099001b17a
Helco <hermann.noll@hotmail.com>
2023-07-31 21:23:26 +0200

    topgun: Add Tamago shell and time handling

---
+++ engines/topgun/plugins/tama/Tamago.cpp
@@ -0,0 +1,147 @@
+#include "Tamago.h"
+
+namespace TopGun {
+
+void Tamago::PropertyTimer::saveLoadWithSerializer(Common::Serializer &ser) {
+	ser.syncAsSint32LE(_value);
+	ser.syncAsSint32LE(_valueDecrease);
+	ser.syncAsSint32LE(_timer);
+	ser.syncAsSint32LE(_timerStart);
+}
+
+static void syncBool(Common::Serializer &ser, bool &value) {
+	byte asByte = value ? 1 : 0;
+	ser.syncAsByte(asByte);
+	value = asByte ? 1 : 0;
+}
+
+void Tamago::BasicTimer::saveLoadWithSerializer(Common::Serializer &ser) {
+	syncBool(ser, _enabled);
+	ser.syncAsSint32LE(_timer);
+	ser.syncAsSint32LE(_timerStart);
+}
+
+void Tamago::ClockTime::saveLoadWithSerializer(Common::Serializer &ser) {
+	ser.syncAsSint32LE(_hour);
+	ser.syncAsSint32LE(_minute);
+}
+
+// this time handling is not very correct by global standards.
+// however it is original to Tamagotchi: CD-ROM
+
+constexpr int kSecondsPerMinute = 60;
+constexpr int kMinutesPerHour = 60;
+constexpr int kHoursPerDay = 24;
+constexpr int kDaysPerWeek = 7;
+constexpr int kMonthsPerYear = 12;
+constexpr int kSecondsPerHour = kSecondsPerMinute * kMinutesPerHour;
+constexpr int kSecondsPerDay = kSecondsPerHour * kHoursPerDay;
+constexpr int kDaysPerMonth[] = {
+	31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+};
+
+static int getDaysInMonth(int year, int mon) {
+	if (mon != 2)
+		return kDaysPerMonth[mon];
+	// not correct by the Gregorian calendar, but 7th Level used this
+	year += 1900;
+	bool isLeapYear = year % 4 == 0 && (year % 100 != 0 || year % 200 == 0);
+	return kDaysPerMonth[mon] + isLeapYear;
+}
+
+Tamago::TimeDateEx::TimeDateEx() {
+	tm_year = 0;
+	tm_mon = 0;
+	tm_mday = 0;
+	tm_wday = 0;
+	tm_hour = 0;
+	tm_min = 0;
+	tm_sec = 0;
+}
+
+Tamago::TimeDateEx::TimeDateEx(TimeDate timeDate) {
+	tm_year = timeDate.tm_year;
+	tm_mon = timeDate.tm_mon;
+	tm_mday = timeDate.tm_mday;
+	tm_wday = timeDate.tm_wday;
+	tm_hour = timeDate.tm_hour;
+	tm_min = timeDate.tm_min;
+	tm_sec = timeDate.tm_sec;
+}
+
+static void wrappingAdd(int &remainder, int &quotient, const int64 add, const int dividend) {
+	remainder += (int)add;
+	quotient += remainder / dividend;
+	remainder %= dividend;
+}
+
+void Tamago::TimeDateEx::advanceBySeconds(int64 seconds) {
+	auto days = (int)(seconds / kSecondsPerDay);
+	seconds -= days * kSecondsPerDay;
+	auto hours = (int)(seconds / kSecondsPerHour);
+	seconds -= hours * kSecondsPerHour;
+	auto minutes = (int)(seconds / kSecondsPerMinute);
+	seconds -= minutes * kSecondsPerMinute;
+
+	wrappingAdd(tm_sec, minutes, seconds, kSecondsPerMinute);
+	wrappingAdd(tm_min, hours, minutes, kMinutesPerHour);
+	wrappingAdd(tm_hour, days, hours, kHoursPerDay);
+	tm_wday = (tm_wday + days) % kDaysPerWeek;
+
+	auto daysInCurMonth = getDaysInMonth(tm_year, tm_mon);
+	while (tm_mday + days > daysInCurMonth) {
+		wrappingAdd(tm_mon, tm_year, 1, kMonthsPerYear);
+		days -= daysInCurMonth - tm_mday + 1;
+		tm_mday = 1;
+		daysInCurMonth = getDaysInMonth(tm_year, tm_mon);
+	}
+	tm_mday += days;
+}
+
+int Tamago::TimeDateEx::compare(const TimeDateEx &other) const {
+	if (tm_year != other.tm_year) return other.tm_year - tm_year;
+	if (tm_mon != other.tm_mon) return other.tm_mon - tm_mon;
+	if (tm_mday != other.tm_mday) return other.tm_mday - tm_mday;
+	if (tm_hour != other.tm_hour) return other.tm_hour - tm_hour;
+	if (tm_min != other.tm_min) return other.tm_min - tm_min;
+	return other.tm_sec - tm_sec;
+}
+
+// these difference functions will only work if this >= other
+
+int64 Tamago::TimeDateEx::differenceInMinutes(const TimeDateEx &other) const {
+	int comparison = compare(other);
+	if (comparison <= 0)
+		return comparison;
+
+	auto diffInDays = tm_mday - other.tm_mday;
+	auto otherMon = other.tm_mon;
+	auto otherYear = other.tm_year;
+	while (tm_year != otherYear && tm_mon != otherMon) {
+		diffInDays += getDaysInMonth(otherYear, otherMon);
+		wrappingAdd(otherMon, otherYear, 1, kMonthsPerYear);
+	}
+
+	const auto diffInHours = diffInDays * kHoursPerDay + tm_hour - other.tm_hour;
+	return diffInHours * kMinutesPerHour + tm_min - other.tm_min;
+}
+
+int64 Tamago::TimeDateEx::differenceInSeconds(const TimeDateEx &other) const {
+	auto diffMinutes = differenceInMinutes(other);
+	return diffMinutes * kSecondsPerMinute + tm_sec - other.tm_sec;
+}
+
+void Tamago::TimeDateEx::saveLoadWithSerializer(Common::Serializer &ser) {
+	ser.syncAsSint32LE(tm_year);
+	ser.syncAsSint32LE(tm_mon);
+	ser.syncAsSint32LE(tm_mday);
+	ser.syncAsSint32LE(tm_wday);
+	ser.syncAsSint32LE(tm_hour);
+	ser.syncAsSint32LE(tm_min);
+	ser.syncAsSint32LE(tm_sec);
+}
+
+Tamago::Tamago(TopGunEngine *engine) : _engine(engine) {
+}
+
+}
---
+++ engines/topgun/plugins/tama/Tamago.h
@@ -0,0 +1,265 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef TOPGUN_TAMAGO_H
+#define TOPGUN_TAMAGO_H
+
+#include "common/str.h"
+#include "common/serializer.h"
+#include "common/system.h"
+
+namespace TopGun {
+class TopGunEngine;
+class Savestate;
+
+enum class TamagoQuery : int32 {
+	kHunger = 101,
+	kHappyness = 102,
+	kDiscipline = 103,
+	kWeight = 104,
+	kCanScold = 108,
+	kCanEat = 110,
+	kCanGiveMedicin = 112,
+	kGoneHome = 113,
+	kCanDaycare = 116,
+	kCanBeWokenUp = 117,
+	kGoneHomeReason = 118,
+	kVType = 120,
+	kAreLightsOff = 121,
+	kFormatAge = 122,
+	kIsValidExtraIntIndex = 123,
+	kGetExtraInt = 124,
+	kStatusFlags = 125,
+	kYears = 126,
+	kPoopCount = 127,
+	kMistakesKind2 = 128,
+	kMistakesKind1 = 129,
+	kGeneration = 130,
+	kWinsShellGame = 131,
+	kStatusMode = 133
+};
+
+enum class TamagoAction : int32 {
+	kGiveMeal = 200,
+	kGiveSnack = 201,
+	kScold = 202,
+	kGiveMedicine = 203,
+	kCleanPoop = 206,
+	kWinGame = 207,
+	kLoseGame = 208,
+	kTimeSpeedUp = 209,
+	kTimeSlowDown = 210,
+	kTimeResetSpeed = 211,
+	kSetAgeString = 213,
+	kSetNextBigUpdateScript = 214,
+	kToggleLights = 215,
+	kHatch = 216,
+	kSetEventScript = 217,
+	kSendHome = 218,
+	kSendToDaycare = 219,
+	kWakeUp = 220,
+	kSetType = 230
+};
+
+enum class TamagoEvent : int32 {
+	kGoneHome = 309,
+	kChangedSleep = 311,
+	kClearedStatusFlag = 313,
+	kChangedType = 314,
+	kChangedStatus = 315,
+	kUnhappyInDaycare = 316
+};
+
+enum class TamagoSender {
+	kPropertyTimer = 1,
+	kUser,
+	kBasicTimer
+};
+
+enum class TamagoGoneHomeReason {
+	kNone = 0,
+	kSentByUser = 1,
+	kNeglected = 2
+};
+
+enum class TamagoType {
+	// names are for first generation
+	kEgg
+};
+
+enum class TamagoVisualType {
+
+};
+
+class Tamago : public Common::Serializable {
+public:
+	Tamago(TopGunEngine *engine);
+
+	static Tamago *createNew(TopGunEngine *engine,
+		const Common::String &nick,
+		int32 generationCount,
+		int32 eventScriptId);
+
+	void update();
+	int32 query(TamagoQuery query, int32 value);
+	int32 action(TamagoAction action, int32 value);
+
+	virtual void saveLoadWithSerializer(Common::Serializer &ser);
+
+private:
+	struct PropertyTimer : public Common::Serializable {
+		int32
+			_value = 0,
+			_valueDecrease = 0,
+			_timer = 0,
+			_timerStart = 0;
+
+		virtual void saveLoadWithSerializer(Common::Serializer &ser);
+	};
+	struct BasicTimer : public Common::Serializable {
+		bool _enabled = false;
+		int32 _timer = 0;
+		int32 _timerStart = 0;
+
+		virtual void saveLoadWithSerializer(Common::Serializer &ser);
+	};
+	struct ClockTime : public Common::Serializable {
+		int32 _hour = 0,
+			_minute = 0;
+
+		virtual void saveLoadWithSerializer(Common::Serializer &ser);
+	};
+	struct TimeDateEx : public TimeDate, public Common::Serializable {
+		TimeDateEx();
+		TimeDateEx(TimeDate timeDate);
+
+		void advanceBySeconds(int64 seconds);
+		int compare(const TimeDateEx &other) const;
+		int64 differenceInSeconds(const TimeDateEx &other) const;
+		int64 differenceInMinutes(const TimeDateEx &other) const;
+
+		virtual void saveLoadWithSerializer(Common::Serializer &ser);
+	};
+	typedef void (Tamago:: *TimerFunction)(TamagoSender sender);
+
+	void send(TamagoEvent event, int32 value);
+	void sendAllStatusMessages();
+
+	void simulateSingleMinute();
+	void simulate(BasicTimer &timer, TimerFunction function);
+	void simulate(PropertyTimer &timer, TimerFunction function);
+	void disable(BasicTimer &timer, bool wasMistake, bool wasMistakeKind1);
+	void simulateSleepCycle();
+	void handleMistake(TamagoSender sender);
+	void handleEvolve(TamagoSender sender);
+	void handleGoingHome(TamagoSender sender);
+	void handleUnhappyInTheDark(TamagoSender sender);
+	void handleHappyness(TamagoSender sender);
+	void handlePreNoHappyness(TamagoSender sender);
+	void handleHunger(TamagoSender sender);
+	void handlePreNoHunger(TamagoSender sender);
+	void handleActingUp(TamagoSender sender);
+	void handleNeglect(TamagoSender sender);
+	void handleTandrum(TamagoSender sender);
+	void handleAutosave(TamagoSender sender);
+
+	void actionGiveMeal();
+	void actionGiveSnack();
+	void actionScold();
+	void actionGiveMedicine();
+	void actionCleanPoop();
+	void actionFinishGame(bool didWin);
+	void actionToggleLights();
+	void actionSendToDaycare();
+	void actionWakeUp();
+
+private:
+	// runtime data
+	TopGunEngine *_engine;
+	bool
+		_isHatched = false,
+		_messagesEnabled = false;
+	int32
+		_timeFactor = 1,
+		_eventScript = 0,
+		_nextBigUpdateScript = 0,
+		_nextBigUpdateMinutes = 0,
+		_ageStringId = 0;
+	BasicTimer _timerAutosave;
+
+	// persistent data
+	Common::String _nick;
+	bool _isAwake = false,
+		_areLighstOn = false,
+		_isInDaycare = false,
+		_canSleep = false,
+		_hasSleepCycle = false;
+	TamagoGoneHomeReason _goneHomeReason = TamagoGoneHomeReason::kNone;
+	TamagoType _type = TamagoType::kEgg;
+	int32
+		_generation = 0,
+		_poopCount = 0,
+		_invDiscipline = 0,
+		_actingUpDuration = 0,
+		_actingUpTimer = 0,
+		_maxTandrums = 0,
+		_unhappyCount = 0,
+		_maxUnhappyCount = 0,
+		_givenMedicine = 0,
+		_necessaryMedicine = 0,
+		_weight = 0,
+		_minWeight = 0,
+		_shellGameChance = 0,
+		_years = 0,
+		_snacks = 0,
+		_totalMistakes = 0,
+		_mistakes1 = 0,
+		_mistakes2 = 0;
+	ClockTime
+		_sleepCycleStart,
+		_sleepCycleEnd;
+	TimeDateEx
+		_lastUpdate,
+		_lastSimulation,
+		_hatched;
+	PropertyTimer
+		_propHunger,
+		_propHappyness,
+		_propNeglect,
+		_propDaycareChance;
+	BasicTimer
+		_timerNoHunger,
+		_timerPreNoHunger,
+		_timerNoHappyness,
+		_timerPreNoHappyness,
+		_timerGoingHome,
+		_timerPoop,
+		_timerTandrum,
+		_timerSick,
+		_timerGrowth,
+		_timerMistake,
+		_timerKickOutOfDaycare,
+		_timerUnhappyInTheDark;
+};
+
+}
+
+#endif
